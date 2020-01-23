9fd5741eb4d8ca048cb1651f881e3c0f04df6f5a
ksemenza27 <ksemenza27@outlook.com>
2020-01-22 19:18:01 -0500

    updated css

--- src/componets/content/Content.js
+++ src/componets/content/Content.js
@@ -5,11 +5,10 @@ import "./content.css"
 const Content = props => {
 
     return (
-
-        
-
         <div className="img-desc-cta">
+            <div className="img-cta">
              <img alt="Space Image" src={props.url}/>
+             </div>
             <div className="desc-wrap">
                 <p className="desc">Description: {props.explanation} </p>
             </div>
